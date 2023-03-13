714c621775e81d24745ea071cc6bbbaf88921c32
Kus Vlz <max1.mmm@yandex.ru>
2023-03-13 14:08:03 +0300

    Добавьте файлы проекта.

---
+++ ClientsDataCollection/Infrastructur/Update.cs
@@ -0,0 +1,29 @@
+﻿using ClientsDataCollection.DataBases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientsDataCollection.Infrastructur
+{
+    internal class Update : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChange(string info)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(info));
+        }
+
+        private List<Clients> clients = new List<Clients>();
+
+        private List<Tarifs> tarifs = new List<Tarifs>();
+
+        public List<Clients> Clients { get => clients; set => clients=value; }
+
+        public List<Tarifs> Tarifs { get => tarifs; set => tarifs=value; }
+    }
+}
